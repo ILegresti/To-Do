f420466cb1c0dec3e79d09261586ac17511e4800
ignacio <ignaciolegresti2@gmail.com>
2023-06-22 21:21:59 -0300

    proyecto to do

---
+++ app.js
@@ -0,0 +1,63 @@
+
+/* AGARRO TODOS LOS ELEMENTOS NECESARIOS PARA PODER TRABAJAR CON ELLOS */
+const input = document.getElementById('ingresar-tarea');
+const boton = document.querySelector('button');
+const listaTareas = document.getElementById('lista-tareas');
+
+function agregarTarea(){
+    if(input.value){
+
+        // si hay un valor en el input, crear tarea
+        let tareaNueva = document.createElement('div');
+        tareaNueva.classList.add('tarea');
+
+        // texto que ingresa desde el input
+        let texto = document.createElement('p');
+        texto.innerText = input.value;
+        tareaNueva.appendChild(texto);
+
+        // crear y agregar el contenedor de los iconos
+        let iconos = document.createElement('div');
+        iconos.classList.add('iconos');
+        tareaNueva.appendChild(iconos);
+
+        // iconos
+        let completar = document.createElement('i');
+        completar.classList.add('bi','bi-check-circle-fill', 'icono-completar');
+        completar.addEventListener('click', completarTarea);
+
+        let eliminar = document.createElement('i');
+        eliminar.classList.add('bi','bi-trash3-fill','icono-eliminar');
+        eliminar.addEventListener('click', eliminarTarea);
+        
+        iconos.append(completar,eliminar);
+
+        //agregar a la lista
+        listaTareas.appendChild(tareaNueva);
+
+
+    }else{
+        alert('Ingresa una tarea')
+    }
+}
+
+function completarTarea(e){
+    let tarea = e.target.parentNode.parentNode;
+    tarea.classList.toggle('completada');
+
+}
+
+function eliminarTarea(e){
+    let tarea = e.target.parentNode.parentNode;
+    tarea.remove();
+
+}
+
+boton.addEventListener('click',agregarTarea);
+
+input.addEventListener('keydown', (e) =>{
+    if(e.key === 'Enter'){
+        agregarTarea();
+    }
+});
+
